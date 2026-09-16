d5076dc79de2c7c3d2b02fd8d237ab08629d43c5
oweniscool-ai <owenglanzman@icloud.com>
2026-09-16 01:12:05 -0700

    Create index.js

---
+++ src/modules/index.js
@@ -0,0 +1,9 @@
+/**
+ * Broccoli Buster - Master Module Loader
+ */
+import { AudioSystem } from './audio.js';
+import { WeaponSystem } from './weapons.js';
+import { BotManager } from './bots.js';
+import { UIManager } from './ui.js';
+
+export { AudioSystem, WeaponSystem, BotManager, UIManager };
